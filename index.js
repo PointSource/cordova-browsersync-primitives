66aade63f14321b202393c05beb4cfd181944496
Andre Asselin <andre.asselin@pointsource.com>
2016-02-17 10:35:11 -0500

    Add isPlatformSupported API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fixATS = require('./fixATS');
 var startBrowserSync = require('./startBrowserSync');
 var getWWWFolder = require('./getWWWFolder');
 var updateConfigXml = require('./updateConfigXml');
+var isPlatformSupported = require('./isPlatformSupported');
 
 module.exports = {
 	addCSP: addCSP,
@@ -13,5 +14,6 @@ module.exports = {
 	fixATS: fixATS,
 	startBrowserSync: startBrowserSync,
 	getWWWFolder: getWWWFolder,
-	updateConfigXml: updateConfigXml
+	updateConfigXml: updateConfigXml,
+	isPlatformSupported: isPlatformSupported
 };
