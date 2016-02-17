7942775c814f532bf7f61dc88ec9b64ee5d0eed4
Andre Asselin <andre.asselin@pointsource.com>
2016-02-17 09:57:46 -0500

    Update API signature on updateConfigXml

--- updateConfigXml.js
+++ updateConfigXml.js
@@ -5,25 +5,27 @@ var fs = require('fs');
 var et = require('elementtree');
 
 
-function updateConfigXml(cordovaDir, platforms, projectName) {
+function updateConfigXml(cordovaDir, platform, projectName, hostedPage) {
 	var CONFIG_LOCATION = {
 		android: 'res/xml',
 		ios: projectName
 	};
 
-	return platforms.forEach(function(platform) {
-		var filename = path.join(cordovaDir, 'platforms', platform, CONFIG_LOCATION[platform], 'config.xml');
+	var filename = path.join(cordovaDir, 'platforms', platform, CONFIG_LOCATION[platform], 'config.xml');
 
-		var configXml = new et.ElementTree(et.XML(fs.readFileSync(filename, "utf-8").replace(/^\uFEFF/, "")));
+	var configXml = new et.ElementTree(et.XML(fs.readFileSync(filename, "utf-8").replace(/^\uFEFF/, "")));
 
-		// Add allow-navigation element so that http:// files will not launch an external browser.
-		var allowNavTag = et.SubElement(configXml.find('.'), 'allow-navigation');
-		allowNavTag.set('href', '*');
+	var contentTag = configXml.find('content[@src]');
+	if (contentTag)
+		contentTag.attrib.src = hostedPage;
 
-		fs.writeFileSync(filename, configXml.write({
-			indent: 4
-		}), "utf-8");
-	});
+	// Add allow-navigation element so that http:// files will not launch an external browser.
+	var allowNavTag = et.SubElement(configXml.find('.'), 'allow-navigation');
+	allowNavTag.set('href', '*');
+
+	fs.writeFileSync(filename, configXml.write({
+		indent: 4
+	}), "utf-8");
 }
 
 
