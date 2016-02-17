e56b2f5c476941edee144f381c3af7b4c52da76a
Andre Asselin <andre.asselin@pointsource.com>
2016-02-17 09:55:47 -0500

    Update API on addCSP

--- addCSP.js
+++ addCSP.js
@@ -4,24 +4,19 @@ var path = require('path');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var Policy = require('csp-parse');
-var getWWWFolder = require('./getWWWFolder');
 
 
-function addCSP(cordovaDir, platforms) {
-	platforms.forEach(function(platform) {
-		var filename = path.join(cordovaDir, getWWWFolder(platform), 'index.html');
-
-		var pageContent = fs.readFileSync(filename, 'utf-8');
-		var $ = cheerio.load(pageContent, {
-			decodeEntities: false
-		});
-		var cspTag = $('meta[http-equiv=Content-Security-Policy]');
-		var policy = new Policy(cspTag.attr('content'));
-		policy.add('default-src', 'ws:');
-		policy.add('default-src', "'unsafe-inline'");
-		cspTag.attr('content', policy.toString());
-		fs.writeFileSync(filename, $.html());
-	}, this);
+function addCSP(filename) {
+	var pageContent = fs.readFileSync(filename, 'utf-8');
+	var $ = cheerio.load(pageContent, {
+		decodeEntities: false
+	});
+	var cspTag = $('meta[http-equiv=Content-Security-Policy]');
+	var policy = new Policy(cspTag.attr('content'));
+	policy.add('default-src', 'ws:');
+	policy.add('default-src', "'unsafe-inline'");
+	cspTag.attr('content', policy.toString());
+	fs.writeFileSync(filename, $.html());
 }
 
 
